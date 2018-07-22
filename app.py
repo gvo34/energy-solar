cd72b451bb98c22ef78c358726c8f897b3d0290b
Guirlyn Olivar <guirlyn@gmail.com>
2018-07-21 18:31:00 -0700

    add dropdown to allow change of history parameter

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 import os
 import io
 import numpy as np
+## Dependencies
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error
+from statsmodels.tsa.ar_model import AR
+from sklearn.preprocessing import StandardScaler
 
 # import keras
 # from keras.preprocessing import image
@@ -16,6 +22,119 @@ app.config['UPLOAD_FOLDER'] = 'Data'
 
 model = None
 graph = None
+
+
+
+# persistence model
+def model_persistence(x):
+    return x
+
+
+@app.route('/Autoregression/<history>')
+def Autoregression(history):
+    print("Autoregression with past lookup of ", history)
+    past_lookup = int(history)
+    ## Preprocessed time serie from Energy Solar consumption
+    filename = "monthdata.csv"
+    output_data = "static/data/output"
+    filepath = os.path.join(output_data,filename)
+    months = pd.read_csv(filepath,index_col=False, header=0)
+    # convert into real dates, set as index for time series
+    months['dates'] = pd.to_datetime(months["YYYYMM"], format="%Y%m")
+    timeserie = months[['dates','Value']]
+    timeserie = timeserie.set_index('dates')
+    series = timeserie
+ 
+    # split dataset
+    X = series.values
+    train, test = X[1:len(X)-past_lookup], X[len(X)-past_lookup:]
+    print("train observations ", len(train), "test observations: ",len(test))
+    # scale
+   
+    X_scaler = StandardScaler().fit(train.reshape(-1, 1))
+    train_scaled = X_scaler.transform(train.reshape(-1, 1))
+    test_scaled = X_scaler.transform(test.reshape(-1, 1))
+    # train autoregression
+    model = AR(train_scaled)
+    model_fit = model.fit()
+    print('Lag: %s' % model_fit.k_ar)
+    print('Coefficients: %s' % model_fit.params)
+    # make predictions
+    predictions = model_fit.predict(start=len(train_scaled), end=len(train_scaled)+len(test_scaled)-1, dynamic=False)
+    for i in range(len(predictions)):
+        print('predicted=%f, expected=%f' % (predictions[i], test_scaled[i]))
+    MSE = mean_squared_error(test_scaled, predictions)
+    # plot results
+    plt.figure()
+    plt.title("Prediction Plot for history of " + history)
+    plt.plot(test_scaled)
+    plt.plot(predictions, color='red')
+    # Save our graph 
+    plt.tight_layout()
+    plt.savefig("static/images/ARmodel.png")
+    score = {"MSE": MSE}
+    print(" done AR ", score)
+    return jsonify(score)
+
+@app.route('/ARHistory/<history>')
+def ARHistory(history):
+    print("Autoregression History with past lookup of ", history)
+    past_lookup = int(history)
+    ## Preprocessed time serie from Energy Solar consumption
+    filename = "monthdata.csv"
+    output_data = "static/data/output"
+    filepath = os.path.join(output_data,filename)
+    months = pd.read_csv(filepath,index_col=False, header=0)
+    # convert into real dates, set as index for time series
+    months['dates'] = pd.to_datetime(months["YYYYMM"], format="%Y%m")
+    timeserie = months[['dates','Value']]
+    timeserie = timeserie.set_index('dates')
+    series = timeserie
+    from statsmodels.tsa.ar_model import AR
+    # split dataset
+    X = series.values
+    train, test = X[1:len(X)-past_lookup], X[len(X)-past_lookup:]
+    print("train observations ", len(train), "test observations: ",len(test))
+    # scale
+    from sklearn.preprocessing import StandardScaler
+    X_scaler = StandardScaler().fit(train.reshape(-1, 1))
+    train_scaled = X_scaler.transform(train.reshape(-1, 1))
+    test_scaled = X_scaler.transform(test.reshape(-1, 1))
+    # train autoregression
+    model = AR(train_scaled)
+    model_fit = model.fit()
+    window = model_fit.k_ar
+    coef = model_fit.params
+    print(f"AR model params window {window}, coef {coef}")
+    # walk forward over time steps in test
+    history = train_scaled[len(train_scaled)-window:]
+    print("History for re-training: ",len(history))
+    history = [history[i] for i in range(len(history))]
+    predictions = list()
+    for t in range(len(test_scaled)):
+        length = len(history)
+        lag = [history[i] for i in range(length-window,length)]
+        yhat = coef[0]
+        for d in range(window):
+            yhat += coef[d+1] * lag[window-d-1]
+        obs = test_scaled[t]
+        predictions.append(yhat)
+        history.append(obs)
+        print('predicted=%f, expected=%f' % (yhat, obs))
+    MSE = mean_squared_error(test_scaled, predictions)
+    print('Test MSE: %.3f' % MSE)
+   # plot results
+    plt.figure()
+    plt.title("Prediction Plot for history of " + history)
+    plt.plot(test_scaled)
+    plt.plot(predictions, color='red')
+    # Save our graph 
+    plt.tight_layout()
+    plt.savefig("static/images/ARmodel_history.png")
+    score = {"MSE": MSE}
+    print(" done ARHistory ", score)
+    return jsonify(score)
+
 
 @app.route('/Linear/<history>')
 def Linear(history):
@@ -58,15 +177,23 @@ def Linear(history):
     model = LinearRegression()
     model.fit(X_train_scaled, y_train_scaled)
 
+    plt.figure()
     plt.scatter(model.predict(X_train_scaled), model.predict(X_train_scaled) - y_train_scaled, c="blue", label="Training Data")
     plt.scatter(model.predict(X_test_scaled), model.predict(X_test_scaled) - y_test_scaled, c="red", label="Testing Data")
     plt.legend()
     plt.hlines(y=0, xmin=y_test_scaled.min(), xmax=y_test_scaled.max())
-    plt.title("Residual Plot for history of ", history)
+    plt.title("Residual Plot for history of " + str(history))
     plt.tight_layout()
     plt.savefig("static/images/LR_residual.png")
     print("DONE LINEAR")
-    return jsonify('Done')
+
+    
+    predictions = model.predict(X_test_scaled)
+    MSE = mean_squared_error(y_test_scaled, predictions)
+    r2 = model.score(X_test_scaled, y_test_scaled)
+    score_linear = {"r2": r2,"MSE": MSE}
+    print(score_linear)
+    return jsonify(score_linear)
 
 
 
--- static/js/logic.js
+++ static/js/logic.js
@@ -1,38 +1,60 @@
-// Creating our initial map object
-// We set the longitude, latitude, and the starting zoom level
-// This gets inserted into the div with an id of 'map'
-// var myMap = L.map('mapit', {
-//     center: [36.77, -119.41],
-//     zoom: 5,
-//   });
-  
-//   // Adding a tile layer (the background map image) to our map
-//   // We use the addTo method to add objects to our map
-//   var access_token =
-//     'pk.eyJ1IjoiZ3Vpcmx5biIsImEiOiJjamh0dzZnaHowaTlnM3BvNGl3NzYwNDQ2In0.fjmZrTxDywSwzCeE6BYKUg';
-//   L.tileLayer(
-//     'https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?' +
-//       'access_token=' +
-//       access_token,
-//   ).addTo(myMap);
-  
-// var history = document.getElementById("#thedropdown");
-// console.log(history);
-
-// var value = history.options[history.selectedIndex].value;
-
-//var value = d3.select("#thedropdown").node().value;
+ 
 var init_value = document.getElementById('thedropdown');
+
 console.log(init_value.options[init_value.selectedIndex].value)
 
 
+function ARHistory(history){
+    console.log("calling Autoregression with history ", history)
+    d3.json(`/Autoregression/${history}`, (error, response) => {
+        if (error) return console.warn(error);
+        
+        // Model score
+        console.log("pass the history of ", history, " Obtained model score of ", response);
+        
+        // Update prediction score
+        var init_score = document.getElementById('prediction_ARH');
+        init_score.innerHTML = response.MSE;
+
+        //refresh image NEED THIS TO WORK
+        //document.getElementById('ARimg').innerHTML = "static/images/AR_residual.png"
+      });
+}
+
+function Autoregression(history){
+    console.log("calling Autoregression ", history)
+    d3.json(`/Autoregression/${history}`, (error, response) => {
+        if (error) return console.warn(error);
+        
+        // Model score
+        console.log("pass the history of ", history, " Obtained model score of ", response);
+        
+        // Update prediction score
+        var init_score = document.getElementById('prediction_AR');
+        init_score.innerHTML = response.MSE;
+
+        //refresh image NEED THIS TO WORK
+        //document.getElementById('ARimg').innerHTML = "static/images/AR_residual.png"
+      });
+}
+
+
+
+
 function Linear(history){
-    console.log("calling with history ", history)
+    console.log("calling Linear Regression with history ", history)
     d3.json(`/Linear/${history}`, (error, response) => {
         if (error) return console.warn(error);
         
-        // draw gauge based on example table loaded in response
-        console.log("pass the history of ", history)
+        // Model score
+        console.log("pass the history of ", history, " Obtained model score of ", response);
+        
+        // Update prediction score
+        var init_score = document.getElementById('prediction_LR');
+        init_score.innerHTML = response.r2;
+
+        //refresh image NEED THIS TO WORK
+        //document.getElementById('LRimg').innerHTML = "static/images/LR_residual.png"
       });
 }
 
@@ -42,6 +64,8 @@ function gethistory(live_value){
     console.log("value changed to ",newvalue );
 
     Linear(newvalue);
+    Autoregression(newvalue);
+    ARHistory(newvalue);
 
 }
 
